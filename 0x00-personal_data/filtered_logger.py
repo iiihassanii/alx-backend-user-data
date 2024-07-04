88c34cdd75257b034c6edb681257076592f74f54
iiihassanii <hassanabdelazim.max@gmail.com>
2024-07-04 12:54:39 +0300

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,9 @@
     Returns:
         _type_: _description_
     """
+
+
+import logging
 import re
 
 
@@ -21,3 +24,34 @@ def filter_datum(fields, redaction, message, separator):
     """
     pattern = f"({'|'.join(fields)})=[^({separator})]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """_summary_
+
+        Args:
+            fields (_type_): _description_
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """_summary_
+
+        Args:
+            record (logging.LogRecord): _description_
+
+        Returns:
+            str: _description_
+        """
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
